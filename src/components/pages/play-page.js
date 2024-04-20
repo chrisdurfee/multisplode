2558a18c7f906a388ecbcf1e22a04f486c03c652
tech_e <chris.durfee@dentalqore.com>
2024-04-19 20:11:57 -0600

    Refactor PlayPage component in play-page.js

--- src/components/pages/play-page.js
+++ src/components/pages/play-page.js
@@ -1,6 +1,11 @@
 import { A, Article, Canvas, Div, Section, Span } from "@base-framework/atoms";
 import { Page } from "./page.js";
 
+function afterSetup()
+{
+	this.progressBar = document.getElementById('level-progress-image');
+}
+
 /**
  * PlayPage
  *
@@ -9,7 +14,7 @@ import { Page } from "./page.js";
  * @returns {object}
  */
 export const PlayPage = () => (
-	new Page([
+	new Page( { afterSetup }, [
 		Section({ class: 'play-container first' }, [
 			Div({ class: 'play-panel' }, [
 				Div({ class: 'fade-layer' }, [
@@ -31,7 +36,7 @@ export const PlayPage = () => (
 							]),
 
 							Section({ class: 'option-group level-number' }, [
-								Div({ id: 'levelNumber', class: 'value title-text' }),
+								Div({ id: 'levelNumber', class: 'value title-text' }, '[[number]]'),
 								Div({ class: 'label title-text' }, 'Level')
 							])
 						]),
@@ -39,7 +44,14 @@ export const PlayPage = () => (
 						Div({ class: 'lower-container' }, [
 							Section({ class: 'option-group level-progress' }, [
 								Div({ id: 'levelProgress', class: 'value progress title-text' }, [
-									Div({ id: 'levelMinimum', class: 'progress-number value title-text' }),
+									Div({ id: 'levelMinimum', class: 'progress-number value title-text' }, '[[minimum]]'),
+									Div({ class: 'progress-bar', html: `<svg id="level-progress-image" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 83.2 83.6" width="83px" height="83px">
+									<style>
+									  .progress-bg{opacity:0.2;fill:#FFFFFF;stroke-width:0px;enable-background:new ;} .progress{fill:none;stroke:#FF4881;stroke-width:4;stroke-miterlimit:10;}
+									</style>
+									<path class="progress-bg" d="M74.7 21L62.2 8.5C50.9-2.8 32.3-2.8 21 8.5L8.5 21c-11.3 11.3-11.3 29.9 0 41.2L21 74.7C32.3 86 50.9 86 62.2 74.7l12.5-12.5C86 50.9 86 32.3 74.7 21zm-3.3 39.1L60.1 71.4c-10.2 10.2-26.9 10.2-37.1 0L11.8 60.1C1.6 49.9 1.6 33.2 11.8 23L23 11.8c10.2-10.2 26.9-10.2 37.1 0L71.4 23c10.2 10.2 10.2 26.9 0 37.1z"/>
+									<path id="progress-line" class="progress" d="M60.82 72.582c-10.606 10.605-27.968 10.605-38.572 0L10.603 60.834c-10.605-10.605-10.605-27.967 0-38.57l11.644-11.645C32.852.013 50.214.013 60.817 10.62l11.75 11.643c10.604 10.605 10.604 27.967 0 38.57l-11.748 11.75"/>
+								  </svg>` })
 									// Svg({ id: 'level-progress-image', xmlns: 'http://www.w3.org/2000/svg', viewBox: '0 0 83.2 83.6', width: '83px', height: '83px' }, [
 									// 	Style('.progress-bg{opacity:0.2;fill:#FFFFFF;stroke-width:0px;enable-background:new ;} .progress{fill:none;stroke:#FF4881;stroke-width:4;stroke-miterlimit:10;}'),
 									// 	Path({ class: 'progress-bg', d: 'M74.7 21L62.2 8.5C50.9-2.8 32.3-2.8 21 8.5L8.5 21c-11.3 11.3-11.3 29.9 0 41.2L21 74.7C32.3 86 50.9 86 62.2 74.7l12.5-12.5C86 50.9 86 32.3 74.7 21zm-3.3 39.1L60.1 71.4c-10.2 10.2-26.9 10.2-37.1 0L11.8 60.1C1.6 49.9 1.6 33.2 11.8 23L23 11.8c10.2-10.2 26.9-10.2 37.1 0L71.4 23c10.2 10.2 10.2 26.9 0 37.1z' }),
