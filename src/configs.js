c597cf86f15e2d6e5e5783ebc360cd6bc0cf8b22
tech_e <chris.durfee@dentalqore.com>
2025-01-23 17:09:34 -0700

    Add TypeScript support and update app configuration; bump version to 0.0.20

--- src/configs.js
+++ src/configs.js
@@ -8,13 +8,23 @@
  */
 export const Configs =
 {
-	appName: 'Multisplode',
+	APP_NAME: 'Multisplode',
 
+	/**
+	 * This will set the size of the app.
+	 *
+	 * @type {object} size
+	 */
 	size: {
 		width: 774,
 		height: 493
 	},
 
+	/**
+	 * This will set the base URL for the app.
+	 *
+	 * @type {object} router
+	 */
 	router:
 	{
 		baseUrl: '/multisplode/',
