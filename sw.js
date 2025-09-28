8028694b45d5358d6a0d79ced58af4890a410270
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-09-28 21:30:45 +0000

    deploy: ef305f642cc2ca31d7b263191952e29a801e5c1a

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@ const APP_NAME = 'multisplode',
  * @const
  * @type {string} version
  */
-version = '0.0.31';
+version = '0.0.33';
 
 /**
  * This will add these files to cache. Add any files you want to
