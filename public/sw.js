a560545fe2ba59b2e96582c190607f5114ba626c
tech_e <chris.durfee@dentalqore.com>
2025-10-18 17:38:40 -0600

    Update service worker version to 0.0.35

--- public/sw.js
+++ public/sw.js
@@ -15,7 +15,7 @@ const APP_NAME = 'multisplode',
  * @const
  * @type {string} version
  */
-version = '0.0.34';
+version = '0.0.35';
 
 /**
  * This will add these files to cache. Add any files you want to
